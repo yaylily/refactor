bac80ee5e301683bc379a2d2a9e2e661e0f05362
modolee <modolee.si@gmail.com>
2024-05-19 17:05:57 +0900

    feat: 사용자 인증 구현
    - AccessToken 인증 미들웨어
    - 내 정보 조회 API

--- src/constants/message.constant.js
+++ src/constants/message.constant.js
@@ -20,12 +20,24 @@ export const MESSAGES = {
         REQURIED: '이름을 입력해 주세요.',
       },
       UNAUTHORIZED: '인증 정보가 유효하지 않습니다.',
+      JWT: {
+        NO_TOKEN: '인증 정보가 없습니다.',
+        NOT_SUPPORTED_TYPE: '지원하지 않는 인증 방식입니다.',
+        EXPIRED: '인증 정보가 만료되었습니다.',
+        NO_USER: '인증 정보와 일치하는 사용자가 없습니다.',
+        INVALID: '인증 정보가 유효하지 않습니다.',
+      },
     },
     SIGN_UP: {
       SUCCEED: '회원가입에 성공했습니다.',
     },
     SIGN_IN: {
       SUCCEED: '로그인에 성공했습니다.',
+    },
+  },
+  USERS: {
+    READ_ME: {
+      SUCCEED: '내 정보 조회에 성공했습니다.',
     },
   },
 };
